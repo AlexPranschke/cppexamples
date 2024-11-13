793e6b2e4327f3647b50ae1d80b82b0e1c4d48f8
Alexpran <alexpranschke@gmail.com>
2024-11-13 15:54:02 +0100

    1

---
+++ imaginary.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Imaginary{
+
+private:
+	int x
+	int y
+public:
+	void get() {
+		cout << x << if (y > 0)"+":"-" << y << "i";
+	}
+
+	Imaginary();
+	~Imaginary();
+	Imaginary operator+(const Imaginary& other) {
+
+	}
+	Imaginary operator-(const Imaginary& other) {
+
+	}
+	Imaginary operator*(const Imaginary& other) {
+
+	}
+	Imaginary operator/(const Imaginary& other) {
+
+	}
+	converti
+}
+
+
+
+
+
+
+
+int main() {
+
+	return 0;
+}
